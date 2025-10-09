a6c04db888e9af11be861eff719822711f85b4c8
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-10-09 20:42:37 +0800

    update etl/config.py

--- etl/config.py
+++ etl/config.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT", 3306)),  
     "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASS"),
+    "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME"),
 }
 
